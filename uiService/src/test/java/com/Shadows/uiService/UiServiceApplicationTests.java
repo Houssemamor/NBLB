a6a9cf5da39db7cc2b0b78e96c1dbd0bf142c3ca
Houssem <hooussem.amor@tek-up.de>
2025-10-28 23:34:44 +0100

    Add initial project structure with Maven configuration and Spring Boot application

---
+++ uiService/src/test/java/com/Shadows/uiService/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shadows.uiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
